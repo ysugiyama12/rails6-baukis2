6a272c6447a33b8dd518ef3638ce600b2d653aa6
ysugiyama12 <yuji9511yuji@gmail.com>
2021-03-02 18:42:57 +0900

    fix

--- apps/baukis2/config/application.rb
+++ apps/baukis2/config/application.rb
@@ -11,7 +11,6 @@ module Baukis2
     config.time_zone = "Tokyo"
     config.i18n.load_path += 
       Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}").to_s]
-    puts config.i18n.load_path
     config.i18n.default_locale = :ja
 
     config.generators do |g|
