35a0e6ec2fe38b755886028561518d503b4dd25d
ysugiyama12 <yuji9511yuji@gmail.com>
2021-03-02 16:57:59 +0900

    add presenter

--- apps/baukis2/config/application.rb
+++ apps/baukis2/config/application.rb
@@ -9,7 +9,9 @@ module Baukis2
     config.load_defaults 6.0
 
     config.time_zone = "Tokyo"
-    config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb, yml}").to_s]
+    config.i18n.load_path += 
+      Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}").to_s]
+    puts config.i18n.load_path
     config.i18n.default_locale = :ja
 
     config.generators do |g|
